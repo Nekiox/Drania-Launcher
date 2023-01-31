7c0218e54594da2e812105a3f515290614cf8d6d
Nekiox <54291626+Nekiox@users.noreply.github.com>
2023-01-31 17:47:56 +0100

    Modifications globales

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -22,9 +22,9 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n' ai pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Bienvenue sur Drania MC", "author": "Drania" },
+            { "message": "Ici on est des pro gamers.", "author": "DraniaMC" },
+            { "message": "DraniaMC va percer.", "author": "Nekiox" }
         ]
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
